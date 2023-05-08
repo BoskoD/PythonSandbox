b85af9ddc25c0d2020d1af5a1e69b76ff0907ea1
Bosko Danilovic <boskodanilovic@outlook.com>
2023-05-08 22:12:46 +0200

    fix typo

--- SQLCRUD/setup.py
+++ SQLCRUD/setup.py
@@ -17,7 +17,7 @@ TABLES['logs'] = (
 )
 
 def create_database():
-    cursor.execute(f"CREATE DATAABSE IF NOT EXISTS {DB_NAME} DEFAULT CHARACTER SET 'utf8'")
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME} DEFAULT CHARACTER SET 'utf8'")
     print(f"Database {DB_NAME} created.")
     
 def create_tables():
